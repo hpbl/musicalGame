2416b406d259696bb9b17adba38f6f8f3c30ac0e
rrb <rrb@cin.ufpe.br>
2018-06-25 16:31:05 -0300

    doing piano scales

--- js/game.js
+++ js/game.js
@@ -3,7 +3,9 @@ let startGame = function() {
 	var height = 480 * 1.5;
 	var game = new Phaser.Game(width, height, Phaser.CANVAS);
 
-	var intervals = 48 // 87
+	var currScaleIndex = 0;
+	var currScale = scales[currScaleIndex];
+	var numberOfIntervals = fullPianoWeak[currScale].length;
 
 	var bird;
      // horizontal bird speed
@@ -19,10 +21,10 @@ let startGame = function() {
 
     var mousePlayed = false;
      
-    var play = function(game){}
+    var play = function(game) {}
      
     play.prototype = {
-		preload:function(){
+		preload:function() {
 			game.load.image("bird", "resources/bird.png");
 			game.load.image("pipe", "resources/pipe.png");
 		},
@@ -30,66 +32,111 @@ let startGame = function() {
 			pipeGroup = game.add.group();
 			score = 0;
 			topScore = localStorage.getItem("topFlappyScore")==null?0:localStorage.getItem("topFlappyScore");
-			scoreText = game.add.text(10,10,"-",{
-				font:"bold 16px Arial"
+			scoreText = game.add.text(10, 10, "-", {
+				font: "bold 16px Arial"
 			});
-			updateScore();
+			scaleText = game.add.text(10, 60, "-", {
+				font: "bold 16px Arial"
+			});
+			updateScoreText();
+			updateScaleText();
 			game.stage.backgroundColor = "#87CEEB";
 			game.stage.disableVisibilityChange = true;
 			game.physics.startSystem(Phaser.Physics.ARCADE);
-			bird = game.add.sprite(80,240,"bird");
+			bird = game.add.sprite(80, 240, "bird");
 			bird.anchor.set(0.5);
 			game.physics.arcade.enable(bird);
-            mouse = game.input.mousePointer;
 			game.time.events.loop(pipeInterval, addPipe); 
 			addPipe();
+
+            mouse = game.input.mousePointer;
+            keyboard = game.input.keyboard;
+
+            keyA = keyboard.addKey(Phaser.Keyboard.A);
+            keyS = keyboard.addKey(Phaser.Keyboard.S);
+            keyD = keyboard.addKey(Phaser.Keyboard.D);
+            keyA.onDown.add(playSound, this);
+            keyS.onDown.add(playSound, this);
+            keyD.onDown.add(playSound, this);
+
+            keyQ = keyboard.addKey(Phaser.Keyboard.Q);
+            keyE = keyboard.addKey(Phaser.Keyboard.E);
+            keyQ.onDown.add(prevScale, this);
+            keyE.onDown.add(nextScale, this);
+
 		},
-		update:function(){
+		update:function() {
 			game.physics.arcade.collide(bird, pipeGroup, die);
-			if(bird.y>game.height){
+			if(bird.y > game.height) {
 				die();
 			}
 			move();
-			if(mouse.isDown && !mousePlayed){
-				mousePlayed = true;
-            	game.input.mouse.requestPointerLock();
-				playSound();
-			}
-			else if (!mouse.isDown){
-				mousePlayed = false;
-			}
+			// if(mouse.isDown && !mousePlayed) {
+			// 	mousePlayed = true;
+			//  game.input.mouse.requestPointerLock();
+			// 	playSound();
+			// }
+			// else if (!mouse.isDown){
+			// 	mousePlayed = false;
+			// }
 		}
 	};
      
-    game.state.add("Play",play);
+    game.state.add("Play", play);
     game.state.start("Play");
      
-    function updateScore(){
-		scoreText.text = "Score: "+score+"\nBest: "+topScore;	
+    function updateScoreText() {
+		scoreText.text = "Score: " + score + "\nBest: " + topScore;
+	}
+
+	function updateScaleText() {
+		scaleText.text = "Scale: " + currScale + '[' + fullPianoWeak[currScale] + ']';
 	}
      
-	function move(){
+	function move() {
 		bird.body.position.y = mouse.position.y
 	}
 
-	function playSound(){
+	function playSound() {
 		//calculo da nota correspondente
-		let noteNumber = Math.floor((game.height - bird.body.position.y)/(game.height/intervals))
-		playNote(noteNumber, 'piano-weak');
+		let noteNumber = Math.floor((game.height - bird.body.position.y) / (game.height / numberOfIntervals))
+		playNote(noteNumber, 'piano-weak', currScale);
+	}
+
+	function prevScale() {
+		currScaleIndex--;
+		if (currScaleIndex < 0) {
+			currScaleIndex = scales.length - 1
+		}
+
+		currScale = scales[currScaleIndex];
+		numberOfIntervals = fullPianoWeak[currScale].length;
+		updateScaleText();
+	}
+
+	function nextScale(){
+		currScaleIndex++;
+		if (currScaleIndex > scales.length - 1) {
+			currScaleIndex = 0
+		}
+
+		currScale = scales[currScaleIndex];
+		numberOfIntervals = fullPianoWeak[currScale].length;
+		updateScaleText();
 	}
 	
-	function addPipe(){
-		var pipeHolePosition = game.rnd.between(50,(height-50)-pipeHole);
-		var upperPipe = new Pipe(game,width,pipeHolePosition-height,-birdSpeed);
+	function addPipe() {
+		var pipeHolePosition = game.rnd.between(50, (height - 50) - pipeHole);
+		var upperPipe = new Pipe(game, width, pipeHolePosition - height, -birdSpeed);
 		game.add.existing(upperPipe);
 		pipeGroup.add(upperPipe);
-		var lowerPipe = new Pipe(game,width,pipeHolePosition+pipeHole,-birdSpeed);
+		var lowerPipe = new Pipe(game, width, pipeHolePosition + pipeHole, -birdSpeed);
 		game.add.existing(lowerPipe);
 		pipeGroup.add(lowerPipe);
 	}
 	
-	function die(){
-		localStorage.setItem("topFlappyScore",Math.max(score,topScore));	
+	function die() {
+		localStorage.setItem("topFlappyScore", Math.max(score,topScore));	
 		game.state.start("Play");	
 	}
 	
@@ -104,12 +151,12 @@ let startGame = function() {
 	Pipe.prototype.constructor = Pipe;
 	
 	Pipe.prototype.update = function() {
-		if(this.x+this.width<bird.x && this.giveScore){
-			score+=0.5;
-			updateScore();
+		if (this.x + this.width < bird.x && this.giveScore) {
+			score += 0.5;
+			updateScoreText();
 			this.giveScore = false;
 		}
-		if(this.x<-this.width){
+		if (this.x < -this.width) {
 			this.destroy();
 		}
 	};	
--- js/sound.js
+++ js/sound.js
@@ -55,19 +55,85 @@ var GminorScale = [
     'G2', 'A2', 'A#2', 'C2', 'D1', 'D#2', 'F2', 'G2'
 ];
 
-var fullPianoWeak = [
-    // 'Bb0', 'B0',
-    // 'C1', 'Db1', 'D1', 'Eb1', 'E1', 'F1', 'Gb1', 'G1', 'Ab1', 'A1', 'Bb1', 'B1',
+// piano
+var scales = [
+    'AMinor',  'AMajor',
+    'BbMinor', 'BbMajor',
+    'BMinor',  'BMajor',
+    'CMinor',  'CMajor',
+    'DbMinor', 'DbMajor',
+    'DMinor',  'DMajor',
+    'EbMinor', 'EbMajor',
+    'EMinor',  'EMajor',
+    'FMinor',  'FMajor',
+    'GbMinor', 'GbMajor',
+    'GMinor',  'GMajor',
+    'AbMinor', 'AbMajor'
+]
+
+var allPianoNotes = [
+                                                                    'Bb0', 'B0',
+    'C1', 'Db1', 'D1', 'Eb1', 'E1', 'F1', 'Gb1', 'G1', 'Ab1', 'A1', 'Bb1', 'B1',
     'C2', 'Db2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2',
     'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3',
     'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4',
     'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5',
-    // 'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'Gb6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6',
-    // 'C7', 'Db7', 'D7', 'Eb7', 'E7', 'F7', 'Gb7', 'G7', 'Ab7', 'A7', 'Bb7', 'B7',
-    // 'C8'
+    'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'Gb6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6',
+    'C7', 'Db7', 'D7', 'Eb7', 'E7', 'F7', 'Gb7', 'G7', 'Ab7', 'A7', 'Bb7', 'B7',
+    'C8'
 ]
+var allNotes = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'];
 
-function playNote(height, instrument) {
+function getScaleNotes(tone, variation) {
+    var jumps = {
+        'Minor': [2, 1, 2, 2, 1, 2],
+        'Major': [2, 2, 1, 2, 2, 2]
+    }
+
+    var currIndex = allNotes.indexOf(tone);
+    var scaleNotes = [allNotes[currIndex]];
+    for (var i in jumps[variation]) {
+        nextCurrIndex = currIndex + jumps[variation][i];
+        if (nextCurrIndex - (allNotes.length - 1) > 0) {
+            currIndex = nextCurrIndex - (allNotes.length - 1) - 1;
+        }
+        else {
+            currIndex = nextCurrIndex;
+        }
+        scaleNotes.push(allNotes[currIndex]);
+    }
+
+    console.log(tone + ', ' + variation + ', ' + scaleNotes)
+    return scaleNotes;
+}
+
+function getNotesFromPiano(scaleNotes) {
+    function isInScale(note, scaleNotes) {
+        for (var i in scaleNotes) {
+            if (note.includes(scaleNotes[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    scalePianoNotes = [];
+    for (var i in allPianoNotes) {
+        if (isInScale(allPianoNotes[i], scaleNotes)) {
+            scalePianoNotes.push(allPianoNotes[i]);
+        }
+    }
+
+    return scalePianoNotes;
+}
+
+var fullPianoWeak = {}
+for (var i in allNotes) {
+    fullPianoWeak[allNotes[i] + 'Minor'] = getNotesFromPiano(getScaleNotes(allNotes[i], 'Minor'));
+    fullPianoWeak[allNotes[i] + 'Major'] = getNotesFromPiano(getScaleNotes(allNotes[i], 'Major'));
+}
+
+function playNote(height, instrument, scale='CMajor') {
     var note;
     switch (instrument) {
         case 'saxophone':
@@ -79,11 +145,11 @@ function playNote(height, instrument) {
             break;
 
         case 'piano-weak':
-            note = fullPianoWeak[height];
+            note = fullPianoWeak[scale][height];
             break;
     }
     console.log(note);
     // current.triggerAttack(note);
-    current.triggerAttackRelease(note, "0.6")
+    current.triggerAttackRelease(note, "1")
 }
 
