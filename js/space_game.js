73fb9a05d598db37d9073269d2e3a1fd43137479
Ricardo Rossiter Barioni <rrb@cin.ufpe.br>
2018-06-30 18:36:47 -0300

    start player object

--- js/space_game.js
+++ js/space_game.js
@@ -5,6 +5,12 @@ let startGame = function() {
 	    type: Phaser.AUTO,
 	    width: 800,
 	    height: 600,
+	    physics: {
+	    	default: "arcade",
+	    	arcade: {
+	    		debug: false
+	    	}
+	    },
 	    scene: {
 	        preload: preload,
 	        create: create,
@@ -18,11 +24,10 @@ let startGame = function() {
 	// player object
 	var player;
 
-	// sound variables
+	// piano sound variables
 	// var currScaleIndex = 0;
 	// var currScale = scales[currScaleIndex];
 	// var numberOfIntervals = fullPianoWeak[currScale].length;
-	// var backgroundMusic;
 
 	var game = new Phaser.Game(config);
 
@@ -30,6 +35,8 @@ let startGame = function() {
 		console.log("preload");
 		
 		this.load.image("background", "resources/textures/background.png");
+		this.load.image("player", "resources/textures/buba.png");
+
 		this.load.audio("backgroundMusic", "resources/audios/background_jazz_am7.mp3");
 	}
 
@@ -40,20 +47,29 @@ let startGame = function() {
 		this.add.image(config.width / 2, config.height / 2, "background");
 
 		// load background music
+  		this.sound.volume = 0.1;
 		this.sound.play("backgroundMusic");
-		// backgroundMusic = this.add.audio("backgroundMusic");
-  //       backgroundMusic.volume = 0.1;
-  //       backgroundMusic.play();
 
         // start screen texts
 		scaleText = this.add.text(10, 60, "-", { font: "bold 16px Arial" });
 		noteText = this.add.text(10, 85, "-", { font: "bold 16px Arial" });
 
-
+		// start player object
+		player = this.add.sprite(100, 100, "player");
+		// this.physics.arcade.enable(player);
 	}
 
 	function update() {
 		console.log("update");
+
+		movePlayer();
+	}
+
+	function updateScreenTexts() {
+	}
+
+	function movePlayer() {
+		player.body.position.y = mouse.position.y;
 	}
 
 }
