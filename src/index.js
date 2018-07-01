8b5a25b7ce477082e1f80c0f9b9ee495f06e0638
Hilton Pintor <hiltonpintor@gmail.com>
2018-07-01 10:39:57 -0300

    adding standar linter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import 'phaser';
+import Phaser from 'phaser'
 
-import { SimpleScene } from './scenes/simple-scene';
-import { Config } from './config';
+import { SimpleScene } from './scenes/simple-scene'
+import { Config } from './config'
 
 const gameConfig = {
   width: Config.width,
   height: Config.height,
   // physics: Config.physics,
   scene: SimpleScene
-};
+}
 
-new Phaser.Game(gameConfig);
+window.game = new Phaser.Game(gameConfig)
