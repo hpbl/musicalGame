8e408e0ff4e998c95ff8017d4156998c7f0b98ff
Hilton Pintor <hiltonpintor@gmail.com>
2018-07-09 20:03:21 -0300

    adding end screen

--- src/scenes/end.js
+++ src/scenes/end.js
@@ -2,28 +2,27 @@ import Phaser from 'phaser'
 
 import { Config } from '../config'
 
-export class HomeScene extends Phaser.Scene {
+export class EndScene extends Phaser.Scene {
   constructor () {
-    super({ key: 'home' })
+    super({ key: 'end' })
   }
 
   preload () {
-    this.load.image('homeBackground', 'assets/homeBackground.png')
-    this.load.image('startButton', 'assets/playButton.png')
-    this.load.image('loadingBackground', 'assets/loadingBackground.png')
+    this.load.image('retryButton', 'assets/retryButton.png')
   }
 
   create () {
     // add background image
-    this.add.image(Config.width / 2, Config.height / 2, 'homeBackground')
+    this.add.image(Config.width / 2, Config.height / 2, 'background')
 
     this.setupButton()
+    this.setupText()
   }
 
   setupButton () {
     let buttonX = Config.width / 2
     let buttonY = Config.height * 0.75
-    this.startBtn = this.add.sprite(buttonX, buttonY, 'startButton').setInteractive()
+    this.startBtn = this.add.sprite(buttonX, buttonY, 'retryButton').setInteractive()
 
     this.startBtn.on('pointerover', (e) => {
       this.input.setDefaultCursor('pointer')
@@ -39,5 +38,18 @@ export class HomeScene extends Phaser.Scene {
 
   pressStart () {
     this.scene.start('game')
+  }
+
+  setupText () {
+    var endText = this.make.text({
+      x: Config.width / 2,
+      y: Config.height * 0.50,
+      text: 'Music is heard on your planet no more',
+      style: {
+        font: '30px monospace',
+        fill: '#49E1D1'
+      }
+    })
+    endText.setOrigin(0.5, 0.5)
   }
 }
